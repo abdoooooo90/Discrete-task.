4c14a495b0fef65c7a5a29bad1b5b4e6f75b0c22
abdoooooo90 <120993266+abdoooooo90@users.noreply.github.com>
2022-12-19 13:31:00 +0200

    Add files via upload

---
+++ perfect number/perfect number/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfect_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the secand number ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            for (int i = num1 ; i <= num2; i++)
+            {
+                int x = 1;
+                int sum = 0;
+                while (x < i)
+                {
+                    if (i % x == 0)
+                        sum = sum + x;
+                    x++;
+                }
+              
+
+                if (sum == i && i !=0)
+                {
+                   
+                    Console.WriteLine("--------");
+                    Console.WriteLine("{0}",i);
+                }
+
+            }
+
+
+
+
+        }
+    }
+}
